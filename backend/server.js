bb1f7191774dcf56c59a81518aa6137e332ef0b6
Gaurav200315 <117560512+Gaurav200315@users.noreply.github.com>
2025-08-29 00:32:00 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -29,12 +29,12 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"))
   );
-} else {
+}else {
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
