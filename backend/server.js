2e5083cfd135ed0d8c5bcd1f43db8ffff2843c9b
Gaurav200315 <117560512+Gaurav200315@users.noreply.github.com>
2025-08-29 12:09:02 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -26,13 +26,13 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
-const __dirname1 = path.resolve(__dirname,"..","frontend","build");
+const __dirname1 = path.resolve(__dirname, "..", "frontend");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
